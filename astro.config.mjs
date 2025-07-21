7b1ed05c903671ecb40a061a5fd2fc9c5d672d71
erekeng <erekeng@gmail.com>
2025-07-21 15:35:30 +0500

    fix:

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static', // ← ҚОС
+  server: {
+    host: "0.0.0.0",
+	port: 4321,
+    allowedHosts: ['woodgroup.kz']
+  }
+})
